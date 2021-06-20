c191bccb89f9ae6254cb57b988098a631335cd1f
Bradley Mackey <bradleymackey@me.com>
2021-06-20 08:10:02 +0100

    Nvim: doc fixes

--- config/nvim/lsp-init.lua
+++ config/nvim/lsp-init.lua
@@ -8,7 +8,7 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
 vim.lsp.diagnostic.on_publish_diagnostics, {
         -- Enable underline, use default values
         underline = false,
-        -- Enable virtual text, override spacing to 4
+        -- Enable virtual text for diagnostics
         virtual_text = {
             spacing = 8
         },
