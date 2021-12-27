9c862e3c8c02b550cae3a3224b696694f1b9076c
Bradley Mackey <bradleymackey@me.com>
2021-12-27 09:33:49 +0000

    Nvim: migrate settings to lua

--- config/nvim/init.vim
+++ config/nvim/init.vim
@@ -5,7 +5,7 @@
 source $HOME/.config/nvim/plugins.vim
 source $HOME/.config/nvim/colors.vim
 source $HOME/.config/nvim/bindings.vim
-source $HOME/.config/nvim/settings.vim
+luafile $HOME/.config/nvim/settings.lua
 luafile $HOME/.config/nvim/completion.lua
 luafile $HOME/.config/nvim/tree-sitter-init.lua
 luafile $HOME/.config/nvim/lsp-init.lua
@@ -116,6 +116,14 @@ hi link DiagnosticFloatingInformation Label
 
 """"""""""""""""""""""""""""""""""""""""
 " # Misc configurations
+
+" Help filetype detection
+autocmd BufRead *.plot set filetype=gnuplot
+autocmd BufRead *.md set filetype=markdown
+autocmd BufRead *.lds set filetype=ld
+autocmd BufRead *.tex set filetype=tex
+autocmd BufRead *.trm set filetype=c
+autocmd BufRead *.xlsx.axlsx set filetype=ruby
 
 nnoremap <silent> <C-n> :NvimTreeToggle<CR>
 nnoremap <silent> <leader>r :NvimTreeRefresh<CR>
---
+++ config/nvim/settings.lua
@@ -0,0 +1,50 @@
+-- BASIC
+vim.o.shell = '/bin/zsh'
+vim.o.encoding = 'utf-8'
+vim.o.ignorecase = true
+-- vim.o.nocompatible = true
+vim.o.hidden = true
+vim.o.ruler = true
+vim.o.ttyfast = true
+-- https://github.com/vim/vim/issues/1735#issuecomment-383353563
+vim.o.lazyredraw = true
+-- http://stackoverflow.com/questions/2158516/delay-before-o-opens-a-new-line
+vim.o.timeoutlen = 300
+vim.opt.shortmess:append({ c = true })
+vim.o.splitright = true
+vim.o.splitbelow = true
+
+-- EDITOR
+vim.o.re = 0
+vim.o.autoindent = true
+vim.o.ai = true
+vim.o.foldlevel = 99 -- open all folds
+vim.o.clipboard = 'unnamed' -- use system clipboard
+vim.o.smartindent = true
+vim.o.expandtab = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.softtabstop = 4
+vim.o.number = true
+vim.o.rnu = true
+vim.o.laststatus = 2
+vim.o.mouse = 'a' -- mouse to click, scroll
+-- allow backspace in insert mode
+vim.opt.backspace = {'indent', 'eol', 'start'}
+vim.o.signcolumn = 'yes'
+
+-- WILDMENU
+vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
+vim.o.wildmenu = true
+vim.o.wildmode = 'list:longest'
+vim.opt.wildignore = {'.hg', '.svn', '*~', '*.png', '*.jpg', '*.gif', '*.heic', '*.settings', 'Thumbs.db', '*.min.js', '*.swp', '*.o', '*.hi', 'Zend', 'vendor'}
+
+-- BETTER SEARCH
+vim.o.incsearch = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.gdefault = true -- (g search) option by default
+
+-- UNDO
+vim.o.undodir = '~/.vimdid'
+vim.o.undofile = true
