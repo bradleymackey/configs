14b75a265414f428954bf7de7d0b85a8db1bf2ba
Bradley Mackey <bradleymackey@me.com>
2022-01-11 10:16:15 +0000

    Nvim: ensure tree sitter grammars are installed

--- config/nvim/lua/tree-sitter-config.lua
+++ config/nvim/lua/tree-sitter-config.lua
@@ -3,7 +3,13 @@
 
 local sitter = require('nvim-treesitter.configs')
 sitter.setup {
+    ensure_installed = 'all',
     highlight = {
-        enable = true
+        enable = true,
+        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+        -- Using this option may slow down your editor, and you may see some duplicate highlights.
+        -- Instead of true it can also be a list of languages
+        additional_vim_regex_highlighting = false
     }
 }
