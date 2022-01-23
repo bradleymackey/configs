df8eaa9998df716cb94667153d28e1dcc735d31a
Bradley Mackey <bradleymackey@me.com>
2022-01-23 10:40:54 +0000

    Nvim: only treesitter some langs

--- config/nvim/lua/tree-sitter-config.lua
+++ config/nvim/lua/tree-sitter-config.lua
@@ -3,7 +3,30 @@
 
 local sitter = require('nvim-treesitter.configs')
 sitter.setup {
-    ensure_installed = 'all',
+    ensure_installed = {
+        'bash',
+        'c',
+        'cpp',
+        'css',
+        'go',
+        'http',
+        'javascript',
+        'json',
+        'jsonc',
+        'llvm',
+        'lua',
+        'markdown',
+        'pug',
+        'python',
+        'r',
+        'rst',
+        'ruby',
+        'rust',
+        'swift',
+        'typescript',
+        'vim',
+        'zig',
+    },
     highlight = {
         enable = true,
         -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
